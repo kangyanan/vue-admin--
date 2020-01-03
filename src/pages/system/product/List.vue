34c4d2b0ed0e56421666322907f44b4e9a2630cc
kangyanan <you@example.com>
2020-01-03 16:00:10 +0800

    顾客管理，员工管理

---
+++ src/pages/system/product/List.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <el-button size="small" type="success" >添加</el-button>
+        <el-button size="small" type="danger">批量删除</el-button>
+</div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<script>
+export default {
+    
+}
+</script>
